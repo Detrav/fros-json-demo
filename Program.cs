9b5d07e9546339ac0986af928cfb706bfec025b4
Detrav <witalyezep@gmail.com>
2022-01-10 13:09:13 +0300

    + added demo code

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using FastReport;
+
+using var report = new Report();
+report.Load("demo.frx");
+
+// update connection from the report
+
+var jsonConnection = report.Dictionary.FindByName("MyConnection") as FastReport.Data.DataConnectionBase;
+if (jsonConnection == null)
+    throw new KeyNotFoundException("MyConnection");
+Console.WriteLine(jsonConnection.ConnectionString);
+var builder = new FastReport.Data.JsonConnection.JsonDataSourceConnectionStringBuilder(jsonConnection.ConnectionString);
+builder.Json = "https://raw.githubusercontent.com/Detrav/fros-json-demo/main/todos.json";
+jsonConnection.ConnectionString = builder.ConnectionString;
+
+// now can process the report
+
+report.Prepare();
+using var export = new FastReport.Export.Image.ImageExport();
+export.Export(report, "demo.png");
